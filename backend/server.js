f81de16d984f1dc3999947eb856e957bc8c2aa95
Hrutav Modha <modhahrutav@gmail.com>
2025-08-06 23:17:15 +0530

    Refactored frontend code, and somewhat modified server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { join } from 'path'
 dotenv.config()
 const app = express()
+app.use(express.static(join(__dirname, '..', 'frontend')))
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'frontend', 'html', 'index.html'))
+})
 app.listen(process.env.PORT, () => {
     console.log(`Backend is running on port ${process.env.PORT}`)
 })
