742f47bec2db2922fc205941f998e8de4c0204fa
Hrutav Modha <modhahrutav@gmail.com>
2025-08-09 10:59:38 +0530

    Fixed few backend bugs. Shifted from ES to CJS syntax

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import { join } from 'path'
-dotenv.config()
+const express = require('express')
+const { join } = require('path')
+const { exec } = require('child_process')
+require('dotenv').config()
 const app = express()
 app.use(express.static(join(__dirname, '..', 'frontend')))
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, '..', 'frontend', 'html', 'index.html'))
 })
-app.listen(process.env.PORT, () => {
+exec(`xdg-open http://${process.env.HOST}:${process.env.PORT}`)
+app.listen(process.env.PORT, process.env.HOST, () => {
     console.log(`Backend is running on port ${process.env.PORT}`)
 })
